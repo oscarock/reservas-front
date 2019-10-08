c85fb9433beb4e75c97852b84e5b97c44c240df5
Oscar Diaz <o-pipe@hotmail.com>
2019-10-07 22:32:10 -0500

    Crea la funcion para mostrar las peliculas con filtro de fechas

--- asset/js/main.js
+++ asset/js/main.js
@@ -4,15 +4,32 @@ new Vue({
         this.getMovies();
     },
     data: {
-        movies: []     
+        movies: [],
+        date_start: "",
+        date_end: "",
     },
     methods: {
         getMovies: function() {
             var urlMovies = 'http://murmuring-hollows-00857.herokuapp.com/api/v1/movies';
-            axios.get(urlMovies).then(function(response){
-                this.movies = response.data;
-                console.log(response.data)
+            axios.get(urlMovies).then(response => {
+                this.movies = response.data
             });
+        },
+        searchMovie: function(){
+            this.date_start = document.querySelector("input[id=datepicker1]").value
+            this.date_end = document.querySelector("input[id=datepicker2]").value
+            var urlMovies = 'http://murmuring-hollows-00857.herokuapp.com/api/v1/movies';
+            axios.get(urlMovies, {
+                params: {
+                    start_date: this.date_start,
+                    end_date: this.date_end
+                }
+              }).then(response => {
+                this.movies = response.data
+                console.log(response.data)
+            }).catch(e => {
+                console.log(e);
+            })
         }
     }
 });
